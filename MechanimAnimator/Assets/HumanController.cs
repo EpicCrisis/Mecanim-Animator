59718207032d82ff274d5424631853f4915beba1
TerrenceChan <roxes2112@yahoo.com>
2017-10-05 18:30:38 +0800

    *created a mechanim animator for Unity

---
+++ MechanimAnimator/Assets/HumanController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+
+	Animator anim;
+	public Transform IKPoi;
+	public float ikPosWeight;
+	public Transform IKHintPoi;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+//		anim.SetFloat ("Forward", Input.GetAxis ("Vertical"));
+//		anim.SetFloat ("Strafe", Input.GetAxis ("Horizontal"));
+	}
+
+	void OnAnimatorIK ()
+	{
+		anim.SetIKPosition (AvatarIKGoal.RightHand, IKPoi.position);
+		anim.SetIKPositionWeight (AvatarIKGoal.RightHand, ikPosWeight);
+		anim.SetIKHintPosition (AvatarIKHint.RightElbow, IKHintPoi.position);
+		anim.SetIKHintPositionWeight (AvatarIKHint.RightElbow, 1f);
+	}
+
+	public void PlayFootStepSFX ()
+	{
+		Debug.Log ("FootStep");
+	}
+}
